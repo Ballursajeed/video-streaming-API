1e89123aedb4c1dc9a41ddf529f52c698eaf8b92
ballursajeed <ballursajeed@gmail.com>
2024-08-26 17:11:25 +0530

    Added JWT Access and Refresh Token in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,10 +1,9 @@
 import mongoose, { Schema } from "mongoose";
-import jsonwebtoken from "jsonwebtoken";
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
 
 const userSchema = new Schema(
-    {
-            
+    {  
         username: {
             type: String,
             required: true,
@@ -60,6 +59,30 @@ userSchema.pre("save", async function(next){
 
 userSchema.methods.isPasswordCorrect = async function (password) {
    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function(){
+    jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullname
+    },
+     process.env.ACCESS_TOKEN_SECRET,
+     {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+     }
+    )
+}
+userSchema.methods.generateRefreshToken = function(){
+    jwt.sign({
+        _id: this._id,
+    },
+     process.env.REFRESH_TOKEN_SECRET,
+     {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+     }
+    )
 }
 
 export const User = mongoose.model("User",userSchema)
