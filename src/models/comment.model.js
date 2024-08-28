0a09fb3deffa1d61352f6a19abfea3c271b38319
ballursajeed <ballursajeed@gmail.com>
2024-08-28 19:46:44 +0530

    Added Playlist Data Model

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,4 +1,4 @@
-import mongoose, { Mongoose, Schema } from "mongoose";
+import mongoose, { Schema } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new mongoose.Schema(
